35df428c6c80a6307cd135bdd34f840a8baa674f
Paulo Lima <paulynho_lima@hotmail.com>
2021-07-09 20:22:34 -0300

    refatorando e corrigindo lint req 1

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
-const getComputer = (computer) => {
-  new Promise((resolve, reject) => {
-    fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${computer}`)
-      .then((response) => response.json())
-      .then((Object) => {
-        Object.results.forEach((result) => {
-          const obj = result;
-          const section = document.querySelector('.items');
-          section.appendChild(createProductItemElement(obj));
-        });
-        resolve();
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
-};
-
 function createProductImageElement(imageSource) {
-  const img = document.createElement("img");
-  img.className = "item__image";
+  const img = document.createElement('img');
+  img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
@@ -32,18 +14,18 @@ function createCustomElement(element, className, innerText) {
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement("section");
-  section.className = "item";
+  section.className = 'item';
 
-  section.appendChild(createCustomElement("span", "item__sku", sku));
-  section.appendChild(createCustomElement("span", "item__title", name));
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(
-    createCustomElement("button", "item__add", "Adicionar ao carrinho!")
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!')
   );
 
   return section;
 }
-
+/*
 function getSkuFromProductItem(item) {
   return item.querySelector("span.item__sku").innerText;
 }
@@ -59,6 +41,38 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener("click", cartItemClickListener);
   return li;
 }
+*/
+
+// USANDO O THEN ////
+/*
+const getComputer = (computer) => {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${computer}`)
+    .then((response) => response.json())
+    .then((Object) => {
+      Object.results.forEach((result) => {
+        const obj = result;
+        const section = document.querySelector('.items');
+        section.appendChild(createProductItemElement(obj));
+      });
+    })
+    .catch((error) => console.log(error, 'erro'));
+};
+*/
+const getComputer = async (computer) => {
+  const response = await fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?q=${computer}`
+  );
+  const object = await response.json();
+  const array = object.results;
+  incrementResults(array);
+};
+const incrementResults = (result) => {
+  result.forEach((entry) => {
+    const obj = entry;
+    const section = document.querySelector('.items');
+    section.appendChild(createProductItemElement(obj));
+  });
+};
 
 window.onload = () => {
   getComputer("computador");
