db5033b1c7aeba5cc605ec09ff0fd7beaea75480
Paulo Lima <paulynho_lima@hotmail.com>
2021-07-12 19:51:44 -0300

    resovido requi. 2 e erro lint

--- script.js
+++ script.js
@@ -12,6 +12,30 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+  function cartItemClickListener() {
+// coloque seu código aqui
+ }
+// requisito 2 add no corrinho
+// usei como referencia a documentação https://www.w3schools.com/jsref/prop_node_parentelement.asp
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+const buscarId = async (event) => {
+  const alvo = event.target.parentElement;
+  const sku = getSkuFromProductItem(alvo);
+  const response = await fetch(`https://api.mercadolibre.com/items/${sku}`);
+  const objet = await response.json();
+  document.querySelector('.cart__items').appendChild(createCartItemElement(objet));
+};
+
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -19,28 +43,27 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  button.addEventListener('click', buscarId);
+  section.appendChild(button);
 
   return section;
 }
-/*
-function getSkuFromProductItem(item) {
-  return item.querySelector("span.item__sku").innerText;
-}
+// requisito 1 add produtos;
+const incrementResults = (result) => {
+  result.forEach((entry) => {
+    const obj = entry;
+    const section = document.querySelector('.items');
+    section.appendChild(createProductItemElement(obj));
+  });
+};
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement("li");
-  li.className = "cart__item";
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener("click", cartItemClickListener);
-  return li;
-}
-*/
-
+const getComputer = async (computer) => {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${computer}`);
+  const object = await response.json();
+  const array = object.results;
+  incrementResults(array);
+};
 // USANDO O THEN ////
 /*
 const getComputer = (computer) => {
@@ -56,20 +79,6 @@ const getComputer = (computer) => {
     .catch((error) => console.log(error, 'erro'));
 };
 */
-
-const incrementResults = (result) => {
-  result.forEach((entry) => {
-    const obj = entry;
-    const section = document.querySelector('.items');
-    section.appendChild(createProductItemElement(obj));
-  });
-};
-const getComputer = async (computer) => {
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${computer}`);
-  const object = await response.json();
-  const array = object.results;
-  incrementResults(array);
-};
 
 window.onload = () => {
   getComputer('computador');
